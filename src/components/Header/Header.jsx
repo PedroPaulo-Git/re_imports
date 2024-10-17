d1e10e6e4a2faf317512dff143e7297ad62b1b48
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-10-17 16:14:21 -0300

    banners added and subbanner design almost completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Logo from '../../assets/LOGO.png'
 import {
   Dialog,
   DialogPanel,
@@ -38,14 +39,14 @@ const Header=() => {
 
   return (
     <header className="bg-white">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between px-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               alt=""
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
+              src={Logo}
+              className="h-20 w-auto"
             />
           </a>
         </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import { XMarkIcon } from "@heroicons/react/20/solid";
 import BannerPod1 from "../../assets/banner.png";
 import BannerPod2 from "../../assets/banner2.png";
 import BannerPod3 from "../../assets/banner3.png";
+import MainBanner from '../../assets/MainBanner.jpg'
 
 const Hero = () => {
   const products = [
@@ -37,6 +38,7 @@ const Hero = () => {
   ];
   return (
     <>
+    
       <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
         <div
           aria-hidden="true"
@@ -62,6 +64,7 @@ const Hero = () => {
             className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
           />
         </div>
+        
         <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
           <p className="text-sm leading-6 text-gray-900">
             <strong className="font-semibold">
@@ -95,16 +98,100 @@ const Hero = () => {
           </button>
         </div>
       </div>
+   
+
+<div>
+  <div className="sm:hidden">
+    <label htmlFor="Tab" className="sr-only">Tab</label>
+
+    <select id="Tab" className="w-full rounded-md border-gray-200">
+      <option>Settings</option>
+      <option>Messages</option>
+      <option>Archive</option>
+      <option select>Notifications</option>
+    </select>
+  </div>
+
+  <div className="hidden sm:block">
+    <div className="border-b border-gray-200">
+      <nav className="-mb-px flex gap-6 justify-center mt-4" aria-label="Tabs">
+        <a
+          href="#"
+          className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        >
+          VAPES
+        </a>
+
+        <a
+          href="#"
+          className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        >
+          PODS
+        </a>
+
+        <a
+          href="#"
+          className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        >
+          NICSALTS
+        </a>
+
+        <a
+          href="#"
+          className="shrink-0 border-b-2 border-red-500 px-1 pb-4 text-sm font-medium text-red-600"
+          aria-current="page"
+        >
+          FREEBASE
+        </a>
+      </nav>
+    </div>
+  </div>
+</div>
+{/* MAIN BANNER */}
+{/*
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+*/}
+
+<section
+  className="overflow-hidden bg-cover bg-top bg-no-repeat"
+  style={{ backgroundImage: `url(${MainBanner})` }}
+>
+  <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+    <div className="text-center ltr:sm:text-left rtl:sm:text-right">
+      <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">Latest Shirts</h2>
+
+      <p className="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore officia corporis quasi
+        doloribus iure architecto quae voluptatum beatae excepturi dolores.
+      </p>
+
+      <div className="mt-4 sm:mt-8">
+        <a
+          href="#"
+          className="inline-block rounded-full bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+        >
+          Get Yours Today
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
       <div>
         <span className="flex items-center mt-2">
           <span className="h-px flex-1 bg-[#111111]"></span>
-          <span className="shrink-0 px-6 text-[#000000] text-center text-3xl sm:text-3xl">
-            Destaques
-          </span>
+          <span className="shrink-0 px-6 text-center text-3xl sm:text-3xl bg-gradient-to-r from-gray-600 via-gray-700 to-gray-900 text-transparent bg-clip-text">
+  Destaques
+</span>
           <span className="h-px flex-1 bg-[#000000]"></span>
         </span>
-        <div className="flex  mx-auto justify-center mt-10">
-          <img className="w-1/3 object-contain" src={BannerPod1} alt="" />
+        <div className="flex  mx-auto justify-center my-10">
+          <img className="w-2/6 object-contain" src={BannerPod1} alt="" />
           <div className="my-auto">
             <p className="text-6xl font-semibold text-zinc-800 max-w-sm">
               Encontre o sabor perfeito para você!
@@ -132,8 +219,8 @@ const Hero = () => {
                 className="group relative inline-block text-sm font-medium text-white focus:outline-none focus:ring"
                 href="#"
               >
-                <span className="absolute inset-0 border border-gray-800 group-active:border-gray-500"></span>
-                <span className="block border border-gray-800 bg-gray-800 px-12 py-3 transition-transform active:border-gray-500 active:bg-gray-500 group-hover:-translate-x-1 group-hover:-translate-y-1">
+                <span className="absolute inset-0 border border-red-800 group-active:border-gray-500"></span>
+                <span className="block border bg-gradient-to-r from-red-500 via-red-600 to-red-700 px-12 py-3 transition-transform active:border-gray-500 active:bg-gray-500 group-hover:-translate-x-1 group-hover:-translate-y-1">
                   Comprar
                 </span>
               </a>
