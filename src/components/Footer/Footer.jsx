41c7ac8b1dbd0c81cb48fe2caccbbd827c062c94
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-10-19 19:51:05 -0300

    images added on shop >>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,21 +5,24 @@ import React from 'react'
     <footer className="bg-white">
         <hr />
   <div className="mx-auto max-w-screen-xl px-4 pb-8 pt-16 sm:px-6 lg:px-8 lg:pt-24">
-    <div className="text-center">
-      <h2 className="text-3xl font-extrabold text-gray-900 sm:text-5xl">Customise Your Product</h2>
+  <div className="text-center">
+  <h2 className="text-3xl font-extrabold text-gray-900 sm:text-5xl">
+    Entre em Contato Conosco
+  </h2>
 
-      <p className="mx-auto mt-4 max-w-sm text-gray-500">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum maiores ipsum eos temporibus
-        ea nihil.
-      </p>
+  <p className="mx-auto mt-4 max-w-sm text-gray-500">
+    Estamos à disposição para responder suas dúvidas e fornecer mais informações sobre nossos produtos.
+  </p>
 
-      <a
-        href="#"
-        className="mt-8 inline-block rounded-full border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
-      >
-        Get Started
-      </a>
-    </div>
+  <a
+    href="https://wa.me/seu_numero_aqui?text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos."
+    className="mt-8 inline-block rounded-full border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+    target="_blank" rel="noreferrer"
+  >
+    Fale Conosco no WhatsApp
+  </a>
+</div>
+
 
     <div
       className="mt-16 border-t border-gray-100 pt-8 sm:flex sm:items-center sm:justify-between lg:mt-24"
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,14 +1,32 @@
 import BannerPod1 from "../../assets/banner.png";
 import BannerPod2 from "../../assets/banner2.png";
 import BannerPod3 from "../../assets/banner3.png";
+import waka from "../../assets/waka.png";
+import ignite150v from "../../assets/ignite.png";
+import waka2 from "../../assets/waka2.png";
+import lost from "../../assets/lost.png";
+import oxbar from "../../assets/oxbar.jpg";
+import oxbarold from "../../assets/oxbar.png";
+
+
+//produtcs
+
+import igniteEX from "../../assets/IGNITEEX.png";
+import ELF6EX from "../../assets/ELF6EX.png";
+import WAKA2EX from "../../assets/WAKA2EX.png";
+import lostmaryEX from "../../assets/lostmaryEX.png";
+import OXBAR8EX from "../../assets/OXBAR8EX.png";
+import ELFWORLDEX from "../../assets/ELFWORLDEX.png";
+import BLVKEX from "../../assets/BLVKEX.png";
+import Blacksheep from "../../assets/Blacksheep.jpg";
 import MainBanner from "../../assets/MainBanner.jpg";
 
-  const products = [
+  const produtosDestaque = [
     {
       id: 1,
       name: "Basic Tee",
       href: "#",
-      imageSrc: BannerPod1,
+      imageSrc: ignite150v,
       imageAlt: "Front of men's Basic Tee in black.",
       price: "$35",
       color: "Black",
@@ -17,7 +35,7 @@ import MainBanner from "../../assets/MainBanner.jpg";
       id: 2,
       name: "Basic Tee",
       href: "#",
-      imageSrc: BannerPod2,
+      imageSrc: lost,
       imageAlt: "Front of men's Basic Tee in black.",
       price: "$35",
       color: "Black",
@@ -26,22 +44,114 @@ import MainBanner from "../../assets/MainBanner.jpg";
       id: 3,
       name: "Basic Tee",
       href: "#",
-      imageSrc: BannerPod3,
+      imageSrc: oxbarold,
       imageAlt: "Front of men's Basic Tee in black.",
       price: "$35",
       color: "Black",
     },
+    {
+        id: 4,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc: waka,
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+      },
+
     // More products...
+  ];
+  
+  const produtos = [
+    {
+      id: 1,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc: igniteEX,
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+    {
+      id: 2,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc: WAKA2EX,
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+    {
+      id: 3,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc: ELF6EX,
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+    {
+        id: 4,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc: Blacksheep,
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+      },
+      {
+        id: 5,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc: OXBAR8EX,
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+      },
+      {
+        id: 6,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc: lostmaryEX,
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+      },
+      {
+        id: 7,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc: BLVKEX,
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+      },
+      {
+        id: 8,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc: ELFWORLDEX,
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+      },
+
   ];
   
 function Shop() {
     return (
+        <>
       <div className="bg-white mx-auto px-20">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Mais Vendidos</h2>
-  
+        <span className="flex items-center mt-2">
+          <span className="h-px flex-1 bg-[#111111]"></span>
+          <span className="shrink-0 px-6 text-center text-3xl sm:text-3xl bg-gradient-to-r from-gray-600 via-gray-700 to-gray-900 text-transparent bg-clip-text">
+            Mais Vendidos
+          </span>
+          <span className="h-px flex-1 bg-[#000000]"></span>
+        </span>
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
+            {produtosDestaque.map((product) => (
               <div key={product.id} className="group relative">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                   <img
@@ -66,7 +176,72 @@ function Shop() {
             ))}
           </div>
         </div>
-      </div>
+    {/* DISCOUNT  */}
+      <section className="overflow-hidden rounded-lg shadow-2xl md:grid md:grid-cols-3 ">
+        <img
+          alt=""
+          src={BannerPod1}
+          className="h-32 w-full object-cover md:h-full"
+        />
+      
+        <div className="p-4 text-center sm:p-6 md:col-span-2 lg:p-8 my-auto">
+          <p className="text-sm font-semibold uppercase tracking-widest">Corra e pegue já o seu</p>
+      
+          <h2 className="mt-6 font-black uppercase">
+            <span className="text-4xl font-black sm:text-5xl lg:text-6xl"> Tenha 20% de desconto </span>
+      
+            <span className="mt-2 block text-sm">Na sua proxima compra</span>
+          </h2>
+      
+          <a
+            className="mt-8 inline-block w-full bg-black py-4 text-sm font-bold uppercase tracking-widest text-white"
+            href="#"
+          >
+            Aproveitar desconto
+          </a>
+      
+          <p className="mt-8 text-xs font-medium uppercase text-gray-400">
+            Oferta válida até 30 de outubro*
+          </p>
+        </div>
+      </section>
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <span className="flex items-center mt-2">
+          <span className="h-px flex-1 bg-[#111111]"></span>
+          <span className="shrink-0 px-6 text-center text-3xl sm:text-3xl bg-gradient-to-r from-gray-600 via-gray-700 to-gray-900 text-transparent bg-clip-text">
+            Mais Produtos
+          </span>
+          <span className="h-px flex-1 bg-[#000000]"></span>
+        </span>
+  
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {produtos.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-white shadow-xl g:aspect-none group-hover:opacity-75 lg:h-80">
+                  <img
+                    alt={product.imageAlt}
+                    src={product.imageSrc}
+                    className="h-full w-full object-contain object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href={product.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        </div>
+      </>
     )
   }
   export default Shop;
